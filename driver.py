fd02b06c8c5d9f38ed83ca15b10fa8425f3d22f2
Jiku Sandilya <jikushandilya@gmail.com>
2023-07-01 22:33:38 +0530

    driver code

---
+++ driver.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("######### Welcome to Assam Travels ############")
+    print("\nPlease Login for Booking and Managing your Bus Tickets => \n")
+    print("Please select an option from below: \n")
+    print("Press 1 for Login")
+    print("Press 2 for Sign Up")
+    print()
+    choice = int(input("Please Enter your choice: "))
